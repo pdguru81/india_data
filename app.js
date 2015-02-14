426a97bdc29ad65c28575516d88e157b7f36e4df
Kapaya <kkatongo@mit.edu>
2015-02-14 00:15:39 -0500

    Working on android login

--- app.js
+++ app.js
@@ -4,9 +4,12 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
+var session = require('express-session');
+var MongoStore = require('connect-mongo')(session);
 
 var routes = require('./routes/index');
 var users = require('./routes/users');
+var android_api = require('./routes/android_api');
 
 var app = express();
 
@@ -20,10 +23,17 @@ app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
+app.use(session({
+    secret: 'indian_asha',
+    store: new MongoStore({
+        mongoose_connection: db
+    })
+}));
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
+app.use('/asha', android_api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
