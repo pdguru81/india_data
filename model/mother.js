6f9952aedc0e004cb89f7aa52ef6baf17527a27f
Tawanda Zimuto <tzimuto1@gmail.com>
2015-02-14 00:39:46 -0500

    Add models

--- model/mother.js
+++ model/mother.js
@@ -7,10 +7,10 @@ This represents an mother object.
 A mother has a name, a phone and an emergency_contact
 **/
 var motherSchema = mongoose.Schema({
+	_asha: {type : Schema.Types.ObjectId, ref: 'asha'},
 	name: String,
 	phone: String,
-	emergency_contact: String,
-
+	emergency_contact: String
 });
 
 var mother = mongoose.model('mother',motherSchema);
