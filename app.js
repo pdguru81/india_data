82f1b6faff790495f2073d5c44b95ac29ecede92
Philip Abel <philipa@mit.edu>
2015-02-14 00:16:49 -0500

    added mongoose and session

--- app.js
+++ app.js
@@ -4,22 +4,46 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-
 var routes = require('./routes/index');
 var users = require('./routes/users');
 
+var mongoose = require('mongoose');
+var session = require('express-session');
+var mongoStore = require('connect-mongo')(session);
+
 var app = express();
+
+
+mongoose.connect('mongodb://localhost/udapradesh_pregnancy_data');
+var db = mongoose.connection;
+db.on('error',function(){
+    console.log("Connection to database failed");
+});
+db.on('open',function(){
+    console.log("Successfully connected!");
+})
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+
+//session
+app.use(cookieParser());
+app.use(session({
+    secret: 'very secret',
+    store: new mongoStore({
+        mongoose_connection:db
+    })
+
+}));
+
+
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
-app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
