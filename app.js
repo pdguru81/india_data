666e54a982791bf3403f93a3427d0ac08d923a58
Philip Abel <philipa@mit.edu>
2015-02-14 06:38:18 -0500

    busszzz

--- app.js
+++ app.js
@@ -59,7 +59,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/users', users);
-app.use('//asha', android_api);
+app.use('asha', android_api);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
