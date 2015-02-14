493204935f660e4770885bed519d9938b89e3501
Philip Abel <philipa@mit.edu>
2015-02-14 00:01:53 -0500

    added controllers

---
+++ model/asha.js
@@ -0,0 +1,19 @@
+var mongoose = require('mongoose');
+var express= require('express');
+
+
+/**
+This represents an asha object.
+An asha belongs to a  Hospital, and  has a name, a phone and a password_hash 
+**/
+var ashaSchema = mongoose.Schema({
+	name: String,
+	phone: String,
+	password_hash: String,
+	hospital: String
+
+});
+
+var asha = mongoose.model('asha',ashaSchema);
+
+module.exports = asha;
